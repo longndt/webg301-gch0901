afb57f8500cd3cfc232655a3efc4d00e76accdac
longndt <55429399+longndt@users.noreply.github.com>
2022-03-31 16:43:21 +0700

    form

---
+++ form/src/Controller/NoteController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Note;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NoteController extends AbstractController
+{
+    #[Route('/', name: 'note_index')]
+    public function noteIndex() {
+        $notes = $this->getDoctrine()->getRepository(Note::class)->findAll();
+        return $this->render('note/index.html.twig', array
+                            (
+                                'notes' => $notes
+                            ));
+    }
+
+    //Cách 1: tạo form trực tiếp trong controller (không recommend)
+    #[Route('/new', name: 'note_new')]
+    public function noteNew(Request $request) {
+        //tạo object cho entity Note
+        $note = new Note;
+        //tạo form bằng form buidler
+        //nội dung của form lấy từ attribute của entity (ngoại trừ id)
+        $form = $this->createFormBuilder($note)
+                     ->add('title', TextType::class)
+                     ->add('content', TextType::class)
+                     ->add('date', DateType::class,
+                      [
+                         'widget' => 'single_text'
+                      ])
+                     ->add('Add', SubmitType::class)
+                     ->getForm();
+        //handle request của form
+        $form->handleRequest($request);
+        //check xem form đã submit hay chưa và có hợp lệ hay không
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($note);
+            $manager->flush();
+            //return $this->redirectToRoute('note_index');
+            return $this->render('note/success.html.twig',
+                                [
+                                    'note' => $note
+                                ]);
+        }
+        //render ra form để nhập liệu
+        return $this->render('note/new.html.twig',
+                            [
+                                'noteForm' => $form->createView()
+                            ]);            
+    }
+
+    //Cách 2: tạo file form riêng và gọi ra ở controller (recommend)
+}
