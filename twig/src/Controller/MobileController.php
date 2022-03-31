76aee3ab89e934ba91aca227541624bab111e8bd
longndt <55429399+longndt@users.noreply.github.com>
2022-03-31 14:54:34 +0700

    twig

--- twig/src/Controller/MobileController.php
+++ twig/src/Controller/MobileController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Mobile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MobileController extends AbstractController
 {
-    #[Route('/mobile', name: 'app_mobile')]
-    public function index(): Response
-    {
-        return $this->render('mobile/index.html.twig', [
-            'controller_name' => 'MobileController',
-        ]);
+    #[Route('/mobile', name: 'mobile_list')]
+    public function viewAllMobile () {
+        //lấy dữ liệu từ bảng Mobile và lưu vào array $mobiles
+        $mobiles = $this->getDoctrine()->getRepository(Mobile::class)->findAll();
+        //render ra file view tương ứng cùng với dữ liệu từ $mobiles
+        return $this->render("mobile/list.html.twig",
+                            [
+                                'mobiles' => $mobiles
+                            ]);
     }
 }
