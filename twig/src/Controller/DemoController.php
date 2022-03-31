39007a96fc4d5eaa41a8a65a587cf428c93ca8fc
longndt <55429399+longndt@users.noreply.github.com>
2022-03-31 14:25:11 +0700

    twig

--- twig/src/Controller/DemoController.php
+++ twig/src/Controller/DemoController.php
@@ -12,4 +12,22 @@ class DemoController extends AbstractController
      public function homepage() {
          return $this->render("demo/home.html.twig");
      }
+
+     #[Route('/demo', name: 'demo')]
+     public function demo() {
+         return $this->render("demo/demo.html.twig");
+     }
+
+     #[Route('/demo1', name: 'demo1')]
+     public function demo1() {
+         $name = "Nguyen Van A";
+         $id = "GCH123456";
+         $email = "gch123456@fpt.edu.vn";
+         return $this->render("demo/demo1.html.twig",
+                            [
+                                'ten' => $name,
+                                'masv' => $id,
+                                'thu' => $email
+                            ]);
+     }
 }
