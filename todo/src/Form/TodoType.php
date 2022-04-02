f254f4ca766f3b4b260e50450df016e030c022e8
longndt <55429399+longndt@users.noreply.github.com>
2022-04-02 16:17:51 +0700

    todo

--- todo/src/Form/TodoType.php
+++ todo/src/Form/TodoType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,43 @@ class TodoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('category')
-            ->add('priority')
-            ->add('due_date')
+            ->add('name', TextType::class,
+            [
+                'attr' => [
+                    'minlength' => 5,
+                    'maxlength' => 50
+                ]
+            ])
+            ->add('description', TextType::class,
+            [
+                'attr' => [
+                    'minlength' => 5,
+                    'maxlength' => 200
+                ]
+            ])
+            ->add('category', ChoiceType::class,
+            [
+                'choices' => [
+                    'Personal' => 'Personal',
+                    'Work' => 'Work',
+                    'Study' => 'Study',
+                    'Family' => 'Family',
+                ]
+            ])
+            ->add('priority', ChoiceType::class,
+            [
+                'choices' => [
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    '4' => '4',
+                    '5' => '5'
+                ]
+            ])
+            ->add('due_date', DateType::class,
+            [
+                'widget' => 'single_text'
+            ])
         ;
     }
 
