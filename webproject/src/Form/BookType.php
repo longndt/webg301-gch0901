f094aad31f141cb748245db31e8927b1cf20da13
longndt <55429399+longndt@users.noreply.github.com>
2022-04-14 16:50:04 +0700

    project

--- webproject/src/Form/BookType.php
+++ webproject/src/Form/BookType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Book;
+use App\Entity\Genre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +17,54 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('author')
-            ->add('price')
-            ->add('date')
-            ->add('image')
-            ->add('genre')
+            ->add('title', TextType::class,
+            [
+                'label' => 'Book Title',
+                'required' => true,
+                'attr' => [
+                    'minlength' => 3,
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('author', TextType::class,
+            [
+                'label' => 'Author Name',
+                'required' => true,
+                'attr' => [
+                    'minlength' => 5,
+                    'maxlength' => 50
+                ]
+            ])
+            ->add('price', MoneyType::class,
+            [
+                'label' => 'Book Price',
+                'required' => true,
+            ])
+            ->add('date', DateType::class,
+            [
+                'label' => 'Published Date',
+                'required' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('image', TextType::class,
+            [
+                'label' => 'Book Cover',
+                'required' => true
+            ])
+            ->add('genre', EntityType::class,
+            [
+                'label' => 'Genre name',
+                'required' => true,
+                'class' => Genre::class,
+                'choice_label' => 'name',
+                'multiple' => false,  
+                'expanded' => false
+                //multiple = true: ManyToMany, OneToMany => có thể chọn nhiều item
+                //multiple = false: OneToOne, ManyToOne => chỉ được chọn 1 item
+                //expanded = true: hiển thị danh sách mở rộng
+                //expanded = false: hiển thị danh sách rút gọn
+            ])
+            ->add('Save', SubmitType::class)
         ;
     }
 
